cf0e0cf871583dba935348c67723f6e6184b2832
afanyu <afanyuemma2002@gmail.com>
2026-03-20 21:40:28 +0200

    update the favicon

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,26 @@ import { defineConfig } from 'vite';
 import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    ViteImageOptimizer({
+      png: { quality: 80 },
+      jpeg: { quality: 80 },
+      jpg: { quality: 80 },
+      webp: { lossless: false, quality: 80 },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
   assetsInclude: ['**/*.svg', '**/*.csv'],
+  build: {
+    assetsInlineLimit: 4096,
+  },
 });
